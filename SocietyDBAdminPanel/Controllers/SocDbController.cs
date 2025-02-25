e3bcbcc5f544bce3285fe8e5b7b6dcef60cb1b21
MilanApex <milandevsoftware@gmail.com>
2025-02-25 23:01:54 +0530

    initial commit

---
+++ SocietyDBAdminPanel/Controllers/SocDbController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocietyDBAdminPanel.Controllers
+{
+    public class SocDbController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
